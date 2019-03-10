77f966850ac1e7c15d1ce7809a15b8d5c2915e13
Prince Raj <princeraj427@gmail.com>
2019-03-10 15:05:46 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package example.com.prince.usamp16;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textview1;
+    TimePicker timepicker;
+    Button changetime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textview1 = (TextView)findViewById(R.id.textView1);
+        timepicker = (TimePicker)findViewById(R.id.timePicker);
+
+        // Uncomment the below line of code for 24 hour view
+        timepicker.setIs24HourView(true);
+        changetime = (Button)findViewById(R.id.button1);
+
+        textview1.setText(getCurrentTime());
+
+        changetime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview1.setText(getCurrentTime());
+            }
+        });
+
+    }
+    public String getCurrentTime(){
+        String currentTime = "Current Time: "+timepicker.getCurrentHour()+":"+timepicker.getCurrentMinute();
+        return currentTime;
+    }
+}
